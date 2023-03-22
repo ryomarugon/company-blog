5f4b3856bf01a2e0a088a609899fa1534a739781
Ryomarugon <murakami.ryoma@geechs.com>
2023-03-22 15:08:22 +0900

    Update sidebar.blade.php

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,19 @@
+@extends('app')
+@section('content')
+<div class="sidebar_fixed">
+    <div class="search">
+        <!-- 検索機能 -->
+        <h3>記事を検索</h3>
+    </div>
+    <div class="featured">
+        <!-- 最新記事or注目記事のピックアップ -->
+        <h3>最新記事</h3>
+    </div>
+    <div class="archive">
+        <!-- 月別アーカイブ -->
+        <h3>月別アーカイブ</h3>
+    </div>
+</div>
+
+
+@endsection
