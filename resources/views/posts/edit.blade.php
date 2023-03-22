6c6689c84bda8c0a933dd8a88c84f4369d0c2529
Ryomarugon <murakami.ryoma@geechs.com>
2023-03-22 14:26:31 +0900

    second commit

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,10 +5,13 @@
     <form action="{{ route('posts.update', $post->id) }}" method="post">
         @csrf
         @method("PUT")
-        <div class="form-group"><input type="text" class="form-control" name="title" value="<?php echo $post->title;?>">
+        <div class="form-group"><input type="text" class="form-control" name="title" value="{{ $post->title }}">
         </div>
         <div class="form-group"><textarea name="body" class="form-control" id="" rows="10"
-                placeholder="文章を入力"><?php echo $post->body;?></textarea></div>
+                placeholder="文章を入力">{{ $post->body }}</textarea>
+        </div>
+        <div class="form-group"><input type="text" class="form-control" name="title" value="{{ $post->tag }}">
+        </div>
         <div class="form-group text-center">
             <button type="submit" class="btn btn-primary">更新</button>
         </div>
