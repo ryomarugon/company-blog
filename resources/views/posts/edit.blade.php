b1189c2e10daed6d61bb6b7675722bd03429a628
Ryomarugon <murakami.ryoma@geechs.com>
2023-03-27 12:06:17 +0900

    Update edit.blade.php

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,6 +5,7 @@
     <form action="{{ route('posts.update', $post->id) }}" method="post">
         @csrf
         @method("PUT")
+<<<<<<< Updated upstream
         <div class="form-group"><input type="text" class="form-control" name="title" value="{{ $post->title }}">
         </div>
         <div class="form-group"><textarea name="body" class="form-control" id="" rows="10"
@@ -13,6 +14,12 @@
         <div class="form-group"><input type="text" class="form-control" name="tag" value="{{ $tag->name }}">
         </div>
         @endforeach
+=======
+        <div class="form-group"><input type="text" class="form-control" name="title" value="{{ $post->title}}">
+        </div>
+        <div class="form-group"><textarea name="body" class="form-control" id="" rows="10"
+                placeholder="文章を入力">{{ $post->body}}</textarea></div>
+>>>>>>> Stashed changes
         <div class="form-group text-center">
             <button type="submit" class="btn btn-primary">更新</button>
         </div>
