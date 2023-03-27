5edd6fbed0c36ba1d1f8404c73cba810b06ccfd7
Ryomarugon <murakami.ryoma@geechs.com>
2023-03-27 10:14:46 +0900

    third commit

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -8,10 +8,11 @@
         <div class="form-group"><input type="text" class="form-control" name="title" value="{{ $post->title }}">
         </div>
         <div class="form-group"><textarea name="body" class="form-control" id="" rows="10"
-                placeholder="文章を入力">{{ $post->body }}</textarea>
+                placeholder="文章を入力">{{ $post->body }}</textarea></div>
+        @foreach($post->tags as $tag)
+        <div class="form-group"><input type="text" class="form-control" name="tag" value="{{ $tag->name }}">
         </div>
-        <div class="form-group"><input type="text" class="form-control" name="title" value="{{ $post->tag }}">
-        </div>
+        @endforeach
         <div class="form-group text-center">
             <button type="submit" class="btn btn-primary">更新</button>
         </div>
